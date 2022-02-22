4946436be3f36e512da90c690d401aa262b2bece
Pravin Jagtap <prjagtap@amd.com>
2022-02-22 16:17:11 +0530

    test2 added

--- hello.cpp
+++ hello.cpp
@@ -17,7 +17,18 @@ void test1(){
     }
 }
 
+void test2(){
+    int ans = add(2,2);
+    if(ans == 4) {
+        std::cout << "test2 passed !!" << std::endl;
+    } else {
+        std::cout << "test2 failed !!" << std::endl;
+    }
+}
+
 int main() {
     std::cout << "Hello World!!" << std::endl;
+    test1();
+    test2();
     return 0;
 }
